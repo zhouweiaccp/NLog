9cf1cf74701281275ed146da85e1ccc060056226
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-01-25 14:29:38 +0100

    Test TypeInitializationException with console

---
+++ src/TypeInitializationExceptionTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Common;
+
+namespace TypeInitializationExceptionTest
+{
+    class Program
+    {
+        //this throws a NLogConfigurationException because of bad config. (like invalid XML)
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
+
+
+        }
+    }
+}
