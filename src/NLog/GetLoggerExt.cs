40f8f6038944e7f8cc7d7e3178217fe526137834
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-02-18 22:25:11 +0100

    init tryout

---
+++ src/NLog/GetLoggerExt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    /// <summary>
+    /// GetLogger with current classname
+    /// </summary>
+    public static class GetLoggerExt
+    {
+        /// <summary>
+        /// Get logger for current class.
+        /// </summary>
+        /// <example>
+        /// this.GetCurrentClassLogger();
+        /// </example>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="current"></param>
+        /// <returns></returns>
+        /// <remarks>Without stackframes</remarks>
+        [CLSCompliant(false)]
+        public static ILogger GetCurrentClassLogger<T>(this T current)
+            where T: class 
+        {
+            return LogManager.GetLogger(typeof(T).FullName);
+        }
+
+        /// <summary>
+        /// Get logger for current class.
+        /// </summary>
+        /// <example>
+        /// this.GetCurrentClassLogger();
+        /// </example>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <remarks>Without stackframes</remarks>
+        [CLSCompliant(false)]
+        public static ILogger GetCurrentClassLogger<T>()
+            where T : class
+        {
+            return LogManager.GetLogger(typeof(T).FullName);
+        }
+
+        /// <summary>
+        /// Get logger for current class.
+        /// </summary>
+        /// <example>
+        /// this.GetCurrentClassLogger();
+        /// </example>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="current"></param>
+        /// <param name="loggerName">name of the logger</param>
+        /// <returns></returns>
+        /// <remarks>Without stackframes</remarks>
+        [CLSCompliant(false)]
+        public static ILogger GetLogger<T>(this T current, string loggerName)
+            where T : class
+        {
+            return LogManager.GetLogger(loggerName);
+        }
+    }
+}
---
+++ tests/NLog.UnitTests/GetLoggerExtTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NLog.UnitTests
+{
+    public class GetLoggerExtTest
+    {
+
+        [Fact]
+        public void GetCurrentClassLoggerExtTest()
+        {
+            var logger = this.GetCurrentClassLogger();
+            Assert.NotNull(logger);
+            Assert.Equal("NLog.UnitTests.GetLoggerExtTest", logger.Name);
+        }
+
+        //TODO type arg of LogManager.GetCurrentClassLogger is used for different purposes
+        //TODO other usage would be nice. But we can't use 'this' is static context.
+        readonly ILogger _logger = GetLoggerExt.GetCurrentClassLogger<GetLoggerExtTest>();
+
+        [Fact]
+        public void GetCurrentClassLoggerExtStaticTest()
+        {
+
+            Assert.NotNull(_logger);
+            Assert.Equal("NLog.UnitTests.GetLoggerExtTest", _logger.Name);
+        }
+    }
+}
